9f28869685d8028002e9539c78d3fd03f38f4f99
Metodi Metodiev <djmmvvmm@gmail.com>
2017-07-13 21:27:03 +0300

    Edit Better Solution

--- ShowCar.cs
+++ ShowCar.cs
@@ -6,20 +6,18 @@ using System.Threading.Tasks;
 
 public class ShowCar: Car
 {
-    //private int stars;
+    private int stars;
 
     public ShowCar(int id, string brand, string model, int yearOfProduction, int horspower, int acceleration, int suspension, int durability) : base(id, brand, model, yearOfProduction, horspower, acceleration, suspension, durability)
     {
         this.Stars = 0;
     }
 
-    public int Stars { get; set; }
-
-    //public int Stars
-    //{
-    //    get { return this.stars; }
-    //    set { this.stars = value; }
-    //}
+    public int Stars
+    {
+        get { return this.stars; }
+        set { this.stars = value; }
+    }
 
     public override string ToString()
     {
